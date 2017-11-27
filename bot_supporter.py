dbb82338f5e85648860499c8cd926a328d72193e
IIITaKeT <33225162+IIITaKeT@users.noreply.github.com>
2017-11-27 07:53:13 +0300

    new bot

---
+++ bot_supporter.py
@@ -0,0 +1,37 @@
+# -*- coding: cp1251 -*-
+from bs4 import BeautifulSoup
+import requests
+import re
+
+products = {}
+req_head = {"user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36"}
+url = "https://www.utkonos.ru"
+
+def ProdAdd(goods_view_box):
+    global products
+    for prod in goods_view_box:
+        prod_name = prod.find('div', class_='goods_view_box-caption').get_text()
+        try:
+            # prod_price = re.findall("\w+",prod.find('div', class_='goods_price-item current').get_text())
+            prod_price = float(str(prod.find('div', class_='goods_price-item current').get_text()).replace(' ', '').replace(',', '.'))
+        except AttributeError:
+            prod_price = [0,0]
+        products.update({prod_name: prod_price})
+
+def GetQuery(message):
+    global url, req_head, products
+    products = {}
+    page = requests.get(url + '/search?query={}'.format(message), headers=req_head)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    if soup.find('div', class_='items_not_found'):
+        return 0
+    ProdAdd(soup.find_all('div', class_='goods_pos_bottom'))
+    return Processing()
+
+def Processing():
+    global products
+    # price_list = [x[1] for x in products]
+    price_list = sorted(products.items(), key=lambda x: x[1])
+    max_price, min_price = price_list[len(price_list)-1], price_list[0]
+    avg_price = round(sum(products.values())/len(products),2)
+    return [min_price, avg_price, max_price]
---
+++ telebot_test.py
@@ -0,0 +1,34 @@
+import telebot
+import time, re
+import bot_supporter
+
+bot_token = "498691939:AAHzmg2ZqaWCqv8vXr4stkcj2eGQ-21PHJs"
+bot = telebot.TeleBot(bot_token)
+
+@bot.message_handler(commands=['start'])
+def start(massage):
+    bot.send_message(massage.chat.id,"Здравствуйте. Я знаю цены на все продукты, могу их вам подсказать")
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    query = re.findall("\w+", message.text)
+    bot.send_message(message.chat.id, 'Окей, ищу')
+    if len(query) >= 2:
+        answer = bot_supporter.GetQuery("{x[0]}+{x[1]}".format(x = query))
+    else:
+        answer = bot_supporter.GetQuery(query[0])
+    if not answer:
+        bot.send_message(message.chat.id, 'Сорян, ничего нет')
+    else:
+        bot.send_message(message.chat.id,
+                 """
+                 Вот что нашел:
+         <b>Средняя цена - {z[1]}р.</b>
+                
+         <b>Минимальная цена - {z[0][1]}р.</b>
+    <i>{z[0][0]}</i>
+         <b>Максимальная цена - {z[2][1]}р.</b>
+    <i>{z[2][0]}</i>
+                 """.format(z = answer), parse_mode="HTML")
+
+bot.polling()
